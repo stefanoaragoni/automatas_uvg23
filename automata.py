e25d9f5a62c281f9bfcf91f6c83860808c52c743
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2023-02-19 10:07:52 -0600

    starting simulation

--- automata.py
+++ automata.py
@@ -1,7 +1,6 @@
 from set import Set
 from estado import Estado
 from simbolo import Simbolo
-from transicion import Transicion
 
 class Automata:
     def __init__(self):
@@ -10,10 +9,3 @@ class Automata:
         self.Simbolos = Set()
         self.estado_inicial = None
         self.transiciones = []
-
-    def Transicion(self, e, s):
-        estados = Set()
-        for transicion in self.transiciones:
-            if transicion.estado_origen == e and transicion.el_simbolo == s:
-                estados.AddItem(transicion.estado_destino)
-        return estados
